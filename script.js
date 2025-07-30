d83bdac5baa1fc04edf10521069926014bc59ae7
Aliaksei Redzko <aleha.redko@gmail.com>
2025-07-30 23:46:06 +0300

    change more error

--- script.js
+++ script.js
@@ -3,17 +3,17 @@
 let offset = 0;
 const sliderLine = document.querySelector('.slider-line');
 
-document.querySelector('.btnRight').addEventListener('click', function(){
+document.querySelector('.btnRight').addEventListener('click', function () {
     offset = offset + 760;
-    if (offset > 3800){
+    if (offset > 3800) {
         offset = 0;
     }
     sliderLine.style.left = - offset + 'px';
 });
 
-document.querySelector('.btnLeft').addEventListener('click', function(){
+document.querySelector('.btnLeft').addEventListener('click', function () {
     offset = offset - 760;
-    if (offset < 0){
+    if (offset < 0) {
         offset = 3800;
     }
     sliderLine.style.left = - offset + 'px';
